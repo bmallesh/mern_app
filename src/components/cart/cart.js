489ea488db0ca5a9b8cd2be28d37c5fe3336e1bf
B mallesh <mallesh@stepv.io>
2019-04-15 07:53:29 +0530

    cart

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react'
 
+import appdata from '../../constant'
+import './cart.css'
 
 class Cart extends Component{
+    constructor(){
+        super()
+        this.state={
+            data : appdata.cart
+        }
+    }
+    removeitem(id){ 
+        appdata.removeitem(id)
+        this.setState({
+            data:appdata.cart
+        })
+    }
 
 
     render(){
         return(
-            <div>my cart</div>
+            <table class="table ">
+            <thead><h6>My Cart ({this.state.data.length})</h6></thead>
+            <tbody>
+            {this.state.data.map((item,i)=>{
+                return(
+                    <tr>
+                    <td><img className="cartImage" src={item.url}  /></td>
+                    <td style={{verticalAlign:"middle"}}>{item.name}</td>
+                    <td style={{verticalAlign:"middle"}}>{item.cost}</td>
+                    <td style={{verticalAlign:"middle"}}>{item.quntity}</td>
+                    <td style={{verticalAlign:"middle"}}><i class='fas fa-trash-alt iconTrash'onClick={()=>{this.removeitem(i)}}></i></td> 
+                  </tr>
+                )
+            })}
+            </tbody>
+            </table>
         )
     }
 }
--- src/constant.js
+++ src/constant.js
@@ -17,5 +17,22 @@ var appdata={
     // set cartdataset(value){
     //     this.cartdata1 = this.cartdata1+value
     // }
+    get cart(){
+        if(localStorage.getItem('cart')){
+            var cart = JSON.parse(localStorage.getItem('cart'))
+            return cart;
+            }else{
+                return []
+            }
+    },
+    removeitem(index){
+        var cart =  JSON.parse(localStorage.getItem('cart'))
+        cart.splice(index,1)
+        if(cart.length==0){
+            localStorage.removeItem("cart")
+        }else{
+            localStorage.setItem("cart",JSON.stringify(cart))
+        }
+    }
 }
 export default appdata;
